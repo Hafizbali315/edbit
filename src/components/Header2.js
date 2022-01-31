799c6f5634f27a61d013d64cae74590749076711
hafizbali315 <hafizbali315@gmail.com>
2022-02-01 02:33:29 +0500

    Dropdowns addded

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,6 +1,12 @@
 import { RiArrowDropDownLine } from 'react-icons/ri'
 import Logo from '../assets/Logo.png'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import LogoE from '../assets/LogoE.png'
+
+import { FaLanguage } from 'react-icons/fa'
+import { AiFillSetting } from 'react-icons/ai'
+import { HiOutlineSupport } from 'react-icons/hi'
+import { RiLogoutCircleRFill } from 'react-icons/ri'
 
 const Header2 = () => {
 	return (
@@ -10,11 +16,62 @@ const Header2 = () => {
 			</div>
 
 			<ul>
-				<li>5 Credits</li>
+				<li className="credits_dropdown">
+					<span>5 Credits</span>
+
+					<div className="credit_dropdown_card">
+						<div className="img_container">
+							<img src={LogoE} alt="LogoL" />
+						</div>
+
+						<div className="text_section">
+							<h5>You Currently have 5 credits left</h5>
+							<p>To buy more credits, subscribe to a paid plan</p>
+						</div>
+					</div>
+				</li>
 				<li>Overview</li>
 				<li>Verify</li>
-				<li>
-					Edu Ongo <RiArrowDropDownLine className="icon" />
+				<li className="setting_menu">
+					<span>
+						Edu Ongo <RiArrowDropDownLine className="icon" />
+					</span>
+
+					<div className="dropdown_container">
+						<div className="setting_menu_dropdown">
+							<ul>
+								<li className="active">
+									<div className="text">
+										Language <RiArrowDropDownLine className="icon" />
+									</div>
+									<div className="icon_container">
+										<FaLanguage className="icon" />
+									</div>
+								</li>
+
+								<li>
+									<div className="text">Settings</div>
+									<div className="icon_container">
+										<AiFillSetting className="icon" />
+									</div>
+								</li>
+
+								<li>
+									<div className="text">Support</div>
+									<div className="icon_container">
+										<HiOutlineSupport className="icon" />
+									</div>
+								</li>
+
+								<li>
+									<div className="text">Logout</div>
+									<div className="icon_container">
+										<RiLogoutCircleRFill className="icon" />
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</li>
 			</ul>
 
